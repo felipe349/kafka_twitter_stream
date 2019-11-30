ffd4243f0af9709f965fcac64828770e55ceb9cb
Felipe Formentin <claiohm@gmail.com>
2019-11-30 00:23:58 -0300

    Initial commit, adding twitter dependency

---
+++ src/main/java/com/github/felipe/kafka/course/ProducerDemo.java
@@ -0,0 +1,51 @@
+package com.github.felipe.kafka.course;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+
+        for (int i = 0; i < 10; i++) {
+
+            String topic = "first_topic";
+            String value = "hello world " + i;
+
+            final ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, value);
+
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        logger.info("Produced with sucess. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp() + "\n" +
+                                "Content: " + record.value() + "\n");
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            });
+
+        }
+
+        producer.close();
+    }
+}
