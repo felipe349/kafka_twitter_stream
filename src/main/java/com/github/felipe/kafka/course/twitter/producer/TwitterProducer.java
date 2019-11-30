fdd0757309a35da2904b7d06e44d26fe611cd82e
Felipe Formentin <claiohm@gmail.com>
2019-11-30 13:25:34 -0300

    Reading tweets in real time

--- src/main/java/com/github/felipe/kafka/course/twitter/producer/TwitterProducer.java
+++ src/main/java/com/github/felipe/kafka/course/twitter/producer/TwitterProducer.java
@@ -10,6 +10,8 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,8 +19,11 @@ import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
+
+    final Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
 
     private String consumerKey;
     private String consumerSecret;
@@ -31,7 +36,43 @@ public class TwitterProducer {
 
     public void run() {
         loadProperties();
-        Client twitterClient = createTwitterClient();
+
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+        Client twitterClient = createTwitterClient(msgQueue);
+        twitterClient.connect();
+
+        while (!twitterClient.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                twitterClient.stop();
+            }
+            if (msg != null) {
+                logger.info(msg);
+            }
+        }
+        logger.info("End of Application");
+    }
+
+
+    public Client createTwitterClient(BlockingQueue<String> msgQueue) {
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        List<String> terms = Lists.newArrayList("#MMA2019");
+        hosebirdEndpoint.trackTerms(terms);
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        Client hosebirdClient = new ClientBuilder()
+                .name("Hosebird-Client-01")
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue))
+                .build();
+
+        return hosebirdClient;
     }
 
     private void loadProperties() {
@@ -47,25 +88,5 @@ public class TwitterProducer {
         consumerSecret = properties.getProperty("consumer.secret");
         token = properties.getProperty("twitter.access.token");
         secret = properties.getProperty("twitter.access.token.secret");
-    }
-
-    public Client createTwitterClient() {
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
-
-        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
-        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
-        List<String> terms = Lists.newArrayList("kafka");
-        hosebirdEndpoint.trackTerms(terms);
-        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
-
-        Client hosebirdClient = new ClientBuilder()
-                .name("Hosebird-Client-01")
-                .hosts(hosebirdHosts)
-                .authentication(hosebirdAuth)
-                .endpoint(hosebirdEndpoint)
-                .processor(new StringDelimitedProcessor(msgQueue))
-                .build();
-
-        return hosebirdClient;
     }
 }
